3924e79ddbe4ce2430417cbce952e95bd76a74c3
pranavv15 <vinodpranav15@gmail.com>
2022-04-18 03:33:44 -0400

    Halfway done

--- Homework3.R
+++ Homework3.R
@@ -1,5 +1,3 @@
-library(dplyr)
-library(glmnet)
 library(mlbench)
 library(caret)
 library(e1071)
@@ -28,31 +26,32 @@ set.seed(420)
 ind <- sample(2, nrow(d), replace = T, prob = c(0.8, 0.2))
 train <- d[ind == 1,]
 test <- d[ind == 2,]
+
+
 # Logistic Regression
 
 mod <- multinom(train$X1~.,data = train,family = 'multinomial')
 
 ##Apply the trained model to the test set
-x_t <- model.matrix(~.,data = xts)
-
-# p<-predict(mod,xts,type="class")
-# posteriprob<-mypred4[,,1]
-# yhat<-matrix(1,nrow(x),1)
-# for(i in 1:nrow(xts))
-# {
-#   yhat[i]<-which.max(posteriprob[i,])
-# }
-# acc<-sum(yhat+2==yts)/nrow(xts)
-# cat("Accuracy on the test set is", acc, "\n")
-
-# Predict
 p <- predict(mod, test, type = 'class')
 confusionMatrix(p, test$X1)
 
 # Neural Network
+train$X. <- as.numeric(train$X.)
+train<- na.omit(train)
 labels <- class.ind(train[,1])
 l <- labels[ind==1]
 myiris<-nnet(train[,-c(1)], labels,
              size=2, rang=0.1,
              decay=5e-4, maxit=200)
+summary(myiris)
 
+test.cl <- function(true, pred) {
+  true <- max.col(true)
+  cres <- max.col(pred)
+  table(true, cres)
+}
+labels2 <- class.ind(test[,1])
+conf<-test.cl(labels2, predict(myiris, test))
+acc<-sum(diag(conf))/sum(conf)
+cat("The accuracy on the test set is", acc,"\n")
